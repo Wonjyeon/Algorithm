4ae06da1c668efe8b3e3c1475e385c8768e09e69
Wonjuyeon <wndus9382@naver.com>
2020-09-09 12:00:17 +0900

    upload 2020 KAKAO codes

---
+++ 2020 KAKAO BLIND RECRUITMENT/외벽 점검.cpp
@@ -0,0 +1,43 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int solution(int n, vector<int> weak, vector<int> dist)
+{
+    int answer = -1;
+    for (int i = 0; i < weak.size(); i++)
+    {
+        int temp = weak[0] + n;
+        for (int j = 1; j < weak.size(); j++)
+        {
+            weak[j - 1] = weak[j];
+        }
+        weak[weak.size() - 1] = temp;
+
+        sort(dist.begin(), dist.end());
+        do
+        {
+            int w = 0, d = 0;
+            for (d = 0; d < dist.size(); d++)
+            {
+                int target = weak[w] + dist[d];
+                while (target >= weak[w])
+                {
+                    w++;
+                    if (w == weak.size())
+                        break;
+                }
+                if (w == weak.size())
+                    break;
+            }
+            if (w == weak.size())
+            {
+                if (answer == -1 || answer > d + 1)
+                    answer = d + 1;
+            }
+        } while (next_permutation(dist.begin(), dist.end()));
+    }
+
+    return answer;
+}
