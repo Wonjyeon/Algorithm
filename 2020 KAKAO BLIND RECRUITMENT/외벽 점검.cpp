a9898b6edcb50737c42554225e8daa6bfdca603c
Won-Juyeon <34434489+Wonjyeon@users.noreply.github.com>
2020-09-09 12:35:34 +0900

    Update 외벽 점검.cpp

--- 2020 KAKAO BLIND RECRUITMENT/외벽 점검.cpp
+++ 2020 KAKAO BLIND RECRUITMENT/외벽 점검.cpp
@@ -3,41 +3,28 @@
 #include <algorithm>
 using namespace std;
 
-int solution(int n, vector<int> weak, vector<int> dist)
-{
+int solution(int n, vector<int> weak, vector<int> dist) {
     int answer = -1;
-    for (int i = 0; i < weak.size(); i++)
-    {
+    sort(dist.begin(), dist.end());
+    for(int i=0; i<weak.size(); i++){
         int temp = weak[0] + n;
-        for (int j = 1; j < weak.size(); j++)
-        {
-            weak[j - 1] = weak[j];
+        for(int j=1; j<weak.size(); j++){
+            weak[j-1] = weak[j];
         }
-        weak[weak.size() - 1] = temp;
-
-        sort(dist.begin(), dist.end());
-        do
-        {
+        weak[weak.size()-1] = temp;
+        do{
             int w = 0, d = 0;
-            for (d = 0; d < dist.size(); d++)
-            {
+            for(d = 0; d<dist.size(); d++){
                 int target = weak[w] + dist[d];
-                while (target >= weak[w])
-                {
+                while(target >= weak[w]){
                     w++;
-                    if (w == weak.size())
-                        break;
+                    if(w == weak.size()) break;
                 }
-                if (w == weak.size())
-                    break;
+                if(w==weak.size()) break;
             }
-            if (w == weak.size())
-            {
-                if (answer == -1 || answer > d + 1)
-                    answer = d + 1;
-            }
-        } while (next_permutation(dist.begin(), dist.end()));
-    }
+            if(w == weak.size() && (answer == -1 || answer > d+1)) answer = d+1;
 
+        }while(next_permutation(dist.begin(), dist.end()));
+    }
     return answer;
 }
